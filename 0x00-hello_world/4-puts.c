816ad08f7565520d1939d6da4df86542b5c65f05
ponzhi <you@example.com>
2022-09-08 11:30:15 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - print: using the string the main function
+ *
+ * Description : using the main function 
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts(*\"Programmin is like building a multilingual puzzle*);	    return (0);
