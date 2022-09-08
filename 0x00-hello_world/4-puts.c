df22a16876eccf6fb80b1d849c1d65e04bfedc09
ponzhi <you@example.com>
2022-09-08 13:19:24 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
+#include  <stdio.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * main - print: using the string the main function
+ * Description : using the main function 
+ * this program prints programming is like building a multilingual puzzle
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	   
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
