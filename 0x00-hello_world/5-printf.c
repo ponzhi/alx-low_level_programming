cfbd658c3f492c75505ac0da47eda28355d44e97
ponzhi <you@example.com>
2022-09-08 17:04:51 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /** 
- * main - prints a text
- * Return: 0 if (Success)
- */
+* main - prints a text
+* Return: 0 if (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
