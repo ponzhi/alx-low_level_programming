aa8299ad05b229d40df61b47956a54d3aa18c382
ponzhi <you@example.com>
2022-09-09 16:38:59 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Prints the number since 0 to 9
+ *
+ *Return: Always (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
