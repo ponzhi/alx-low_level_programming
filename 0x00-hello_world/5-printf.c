c7281422afd78ae7000ebb8cd3c0a8eadf03f853
ponzhi <you@example.com>
2022-09-10 02:46:07 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 /** 
  *main - prints the phrase with proper grammar
- *but the outcome is a piece of art,
- *Return: 0 if exited properly, non-zero otherwise 
+ *
+ *Return: Always (success)
  */
 int main(void)
 {
