a69af4265462c4279acb369362929a2a392cf481
ponzhi <you@example.com>
2022-09-13 16:35:53 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ *main - print the lower alphabets
+ *
+ *Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
