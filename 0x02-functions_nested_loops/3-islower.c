e336757fb77c17f2e99e213db3a24ab8bc7a8a6b
ponzhi <you@example.com>
2022-09-13 18:03:51 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *_islower -> checks if the character is lowercase
- *@c: is the int that will use for the argument of the function 
+ *@c: is the int that will use for the argument of the function
  *Return: return 1 and 0 depending on condition
  */
 
