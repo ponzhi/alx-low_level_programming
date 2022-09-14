399b6ea786412539e8e68461f21b485e68e74879
ponzhi <you@example.com>
2022-09-13 20:18:23 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -31,8 +31,8 @@ void jack_bauer(void)
 			else if (j >= 10)
 			{
 				_putchar(':');
-			       	_putchar((j / 10) + '0');
-			       	_putchar((j % 10) + '0');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
 		}
