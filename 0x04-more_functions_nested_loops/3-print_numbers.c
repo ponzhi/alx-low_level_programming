aedfc4b0b4e11ef2f74e61aa3eab3519e9d8384b
ponzhi <you@example.com>
2022-09-15 14:22:02 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+		
+		_putchar('\n');
+}
