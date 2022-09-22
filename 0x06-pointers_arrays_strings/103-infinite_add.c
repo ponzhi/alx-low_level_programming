bf928d02e9de155d282ca2c69d4ab29b26544113
ponzhi <you@example.com>
2022-09-22 16:37:33 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *  * infinite_add - adds two numbers
  *
@@ -24,9 +22,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
-		;
 	for (j = 0; n2[j]; j++)
-		;
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
