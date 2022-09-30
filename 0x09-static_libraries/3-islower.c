1ecd4eea91e2113318d87451a223380402f3dcaf
ponzhi <you@example.com>
2022-09-30 14:35:58 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_islower -> checks if the character is lowercase
+ *@c: is the int that will use for the argument of the function
+ *Return: return 1 and 0 depending on condition
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _abs -> return absolute value of number
+ *  @n: parameter
+ *  Return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+	return (n);
+	}
+	else
+	return (n * -1);
+}
