9e0ad0c3c76686ff93f645c4df3f397d48cfe664
ponzhi <you@example.com>
2022-10-10 15:11:43 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,10 @@
  * Description: This struct is for dog
  */
 
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	else
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ int _strlen(char *str)
 	while (*str++)
 		len++;
 
-	return (lem);
+	return (len);
 }
 
 /**
@@ -36,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 	dog_t *doggo;
 
 	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL)
+		return (NULL);
 	
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)
