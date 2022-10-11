ca4945f32d10aea73b40174738cab959d456775a
ponzhi <you@example.com>
2022-10-11 13:00:01 -0700

    all Task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ * was compilied from. followed by a new line.
+ * Return: Alwayss 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
