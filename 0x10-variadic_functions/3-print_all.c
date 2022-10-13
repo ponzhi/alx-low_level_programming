878fc0dab7f07fdae0eeda06698a3223b4f04c84
ponzhi <you@example.com>
2022-10-13 15:03:48 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -43,7 +43,7 @@ void print_all(const char * const format, ...)
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
 			{
-				printf("nil)");
+				printf("(nil)");
 				break;
 			}
 			printf("%s", str);
